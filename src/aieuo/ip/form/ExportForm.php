f637b8ac46c465bd128dc0c3871b86e1db1453b4
aieuo <35859665+aieuo@users.noreply.github.com>
2019-01-13 12:25:12 +0900

    IFをファイル出力できるように

---
+++ src/aieuo/ip/form/ExportForm.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace aieuo\ip\form;
+
+use aieuo\ip\ifPlugin;
+use aieuo\ip\Session;
+use aieuo\ip\utils\Messages;
+
+class ExportForm {
+    public function getExportForm($mes = ""){
+        $data = [
+            "type" => "custom_form",
+            "title" => "共有用ファイル作成",
+            "content" => [
+                Elements::getInput(($mes === "" ? "" : $mes."\n")."タイトル"),
+                Elements::getInput("作成者"),
+                Elements::getInput("説明"),
+                Elements::getToggle("キャンセル")
+            ]
+        ];
+        $json = Form::encodeJson($data);
+        return $json;
+    }
+
+    public function onExport($player, $data) {
+        $session = $player->ifSession;
+        if($data === null) {
+            $session->setValid(false, false);
+            return;
+        }
+        $type = $session->getIfType();
+        if($type === Session::BLOCK) {
+            $manager = ifPlugin::getInstance()->getBlockManager();
+            $option = [];
+        }elseif($type === Session::COMMAND) {
+            $manager = ifPlugin::getInstance()->getCommandManager();
+            $option = ["desc" => $session->getData("description"), "perm" => $session->getData("permission")];
+        }elseif($type === Session::EVENT) {
+            $manager = ifPlugin::getInstance()->getEventManager();
+            $option = ["eventname" => $session->getData("eventname")];
+        }
+        $key = $session->getData("if_key");
+        $datas = $manager->get($key, $option);
+        if($data[3]) {
+            $mes = Messages::createMessage($datas["if"], $datas["match"], $datas["else"]);
+            $form = (new Form())->getEditIfForm($mes);
+            Form::sendForm($player, $form, new Form(), "onEditIf");
+        	return;
+        }
+        if($data[0] == "" or $data[1] == "" or $data[2] == "") {
+            $form = $this->getExportForm("§c必要事項を記入してください§f");
+            Form::sendForm($player, $form, $this, "onExport");
+            $player->sendMessage("必要事項を入力してください");
+            return;
+        }
+        $export = [
+        	"name" => $data[0],
+        	"author" => $data[1],
+        	"details" => $data[2],
+        	"ifs" => [
+        		$key => $datas + [
+        			"type" => $type
+        		] + [
+        			"options" => $option
+        		]
+        	]
+        ];
+        $filename = $data[0]."_".$data[1]."_".$type."_".$key.".json";
+        $path = ifPlugin::getInstance()->getDataFolder()."exports/".$filename;
+        file_put_contents($path, json_encode($export, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+        $player->sendMessage($filename."として保存しました");
+        $session->setValid(false);
+    }
+}
