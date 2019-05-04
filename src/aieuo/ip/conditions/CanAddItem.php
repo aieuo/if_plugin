14e390253c91b61e0fadf169d8cd1159c1f8c92c
aieuo <35859665+aieuo@users.noreply.github.com>
2019-05-04 12:12:43 +0900

    多言語化

--- src/aieuo/ip/conditions/CanAddItem.php
+++ src/aieuo/ip/conditions/CanAddItem.php
@@ -7,23 +7,25 @@ use pocketmine\item\Item;
 use aieuo\ip\form\Form;
 use aieuo\ip\form\Elements;
 
+use aieuo\ip\utils\Language;
+
 class CanAddItem extends TypeItem {
 
 	protected $id = self::CAN_ADD_ITEM;
-	protected $name = "インベントリにアイテムを追加できるか";
-	protected $description = "インベントリにidが§7<id>§fのアイテムを§7<count>§f個追加できるスペースがあるなら";
+	protected $name = "@condition.canadditem.name";
+	protected $description = "@condition.canadditem.description";
 
 	public function getMessage() {
 		$item = $this->getItem();
 		if(!($item instanceof Item)) return false;
-		return "インベントリに(".$item->getId().":".$item->getDamage().")"."を".$item->getCount()."個追加できるなら";
+		return Language::get("condition.canadditem.detail", [$item->getId(), $item->getDamage(), $item->getCount()]);
 	}
 
 	public function check() {
 		$player = $this->getPlayer();
 		$item = $this->getItem();
 		if(!($item instanceof Item)) {
-			$player->sendMessage("§c[".$this->getName()."] 正しく入力できていません");
+			$player->sendMessage(Language::get("input.invalid", [$this->getName()]));
 			return self::ERROR;
 		}
         if($player->getInventory()->canAddItem($item)) return self::MATCHED;
