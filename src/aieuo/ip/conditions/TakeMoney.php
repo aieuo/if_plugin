5c6165e683def60f969ab65ac3c242c054e07deb
aieuo <35859665+aieuo@users.noreply.github.com>
2019-08-06 23:01:52 +0900

    文字化けしてた

--- src/aieuo/ip/conditions/TakeMoney.php
+++ src/aieuo/ip/conditions/TakeMoney.php
@@ -5,29 +5,30 @@ namespace aieuo\ip\conditions;
 use aieuo\ip\ifPlugin;
 
 use aieuo\ip\utils\Language;
+use pocketmine\utils\TextFormat;
 
 class TakeMoney extends TypeMoney {
 
-	protected $id = self::TAKEMONEY;
+    protected $id = self::TAKEMONEY;
     protected $name = "@condition.takemoney.name";
     protected $description = "@condition.takemoney.description";
 
-	public function getMessage() {
-		return Language::get("condition.takemoney.detail", [$this->getAmount()]);
-	}
+    public function getMessage() {
+        return Language::get("condition.takemoney.detail", [$this->getAmount()]);
+    }
 
-	public function check() {
-		$player = $this->getPlayer();
+    public function check() {
+        $player = $this->getPlayer();
         $economy = ifPlugin::getInstance()->getEconomy();
-        if($economy === null) {
-            $player->sendMessage("§c".Language::get("economy.notfound"));
+        if ($economy === null) {
+            $player->sendMessage(TextFormat::RED.Language::get("economy.notfound"));
             return self::ERROR;
         }
         $mymoney = $economy->getMoney($player->getName());
-        if($mymoney >= $this->getAmount()){
+        if ($mymoney >= $this->getAmount()) {
             $economy->takeMoney($player->getName(), $this->getAmount());
             return self::MATCHED;
         }
         return self::NOT_MATCHED;
-	}
+    }
 }
