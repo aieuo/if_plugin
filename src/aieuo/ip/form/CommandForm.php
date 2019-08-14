a1069d4b76afc29446fa54639ab77b0580ef2365
aieuo <35859665+aieuo@users.noreply.github.com>
2019-08-15 00:09:11 +0900

    メッセージ取り出し

--- src/aieuo/ip/form/CommandForm.php
+++ src/aieuo/ip/form/CommandForm.php
@@ -6,21 +6,22 @@ use aieuo\ip\ifPlugin;
 use aieuo\ip\Session;
 use aieuo\ip\utils\Messages;
 use aieuo\ip\form\Form;
+use aieuo\ip\utils\Language;
 
 class CommandForm {
     public function getSelectActionForm() {
         $data = [
             "type" => "form",
-            "title" => "command > 操作選択",
-            "content" => "§7ボタンを押してください",
+            "title" => Language::get("form.command.action.title"),
+            "content" => Language::get("form.selectButton"),
             "buttons" => [
-                Elements::getButton("追加する"),
-                Elements::getButton("コマンドだけ追加する"),
-                Elements::getButton("編集する"),
-                Elements::getButton("削除する"),
-                Elements::getButton("追加したコマンド一覧"),
-                Elements::getButton("キャンセルする"),
-                Elements::getButton("ひとつ前の画面に戻る")
+                Elements::getButton(Language::get("form.action.add")),
+                Elements::getButton(Language::get("form.command.action.addOnlyCommand")),
+                Elements::getButton(Language::get("form.action.edit")),
+                Elements::getButton(Language::get("form.action.delete")),
+                Elements::getButton(Language::get("form.command.action.commandList")),
+                Elements::getButton(Language::get("form.cancel")),
+                Elements::getButton(Language::get("form.back"))
             ]
         ];
         $json = Form::encodeJson($data);
@@ -28,7 +29,7 @@ class CommandForm {
     }
 
     public function onSelectAction($player, $data) {
-        if($data === null) return;
+        if ($data === null) return;
         $session = Session::get($player);
         switch ($data) {
             case 0:
@@ -49,7 +50,7 @@ class CommandForm {
                 return;
             case 5:
                 $session->setValid(false);
-                $player->sendMessage("キャンセルしました");
+                $player->sendMessage(Language::get("form.cancelled"));
                 return;
             case 6:
                 $form = (new Form())->getSelectIfTypeForm();
@@ -77,12 +78,12 @@ class CommandForm {
     public function getAddCommandForm($mes = "") {
         $data = [
             "type" => "custom_form",
-            "title" => "command > コマンド追加",
+            "title" => Language::get("form.command.addCommand.title"),
             "content" => [
-                Elements::getInput(($mes !== "" ? $mes."\n" : "")."追加するコマンドの名前", "最初の/を外して"),
-                Elements::getInput("コマンドの説明"),
-                Elements::getDropdown("権限", ["opだけ", "全員使える"], 1),
-                Elements::getToggle("キャンセル")
+                Elements::getInput(($mes !== "" ? $mes."\n" : "").Language::get("form.command.addCommand.content0"), Language::get("form.command.addCommand.content0.placeholder")),
+                Elements::getInput(Language::get("form.command.addCommand.content1")),
+                Elements::getDropdown(Language::get("form.command.permission"), [Language::get("form.command.permission.op"), Language::get("form.command.permission.everyone")], 0),
+                Elements::getToggle(Language::get("form.cancel"))
             ]
         ];
         $json = Form::encodeJson($data);
@@ -91,38 +92,38 @@ class CommandForm {
 
     public function onAddCommand($player, $data) {
         $session = Session::get($player);
-        if($data === null) {
+        if ($data === null) {
             $session->setValid(false, false);
             return;
         }
         $manager = ifPlugin::getInstance()->getCommandManager();
-        if($data[3]) {
+        if ($data[3]) {
             $form = $this->getSelectActionForm();
             Form::sendForm($player, $form, $this, "onSelectAction");
             return;
         }
-        if($data[0] === ""){
+        if ($data[0] === "") {
             $form = $this->getAddCommandForm("§c必要事項を入力してください§f");
             Form::sendForm($player, $form, $this, "onAddCommand");
             $player->sendMessage("必要事項を入力してください");
             return;
         }
-        if($manager->exists($data[0])){
+        if ($manager->exists($data[0])) {
             $form = $this->getAddCommandForm("§cそのコマンドは既に使用されています§f");
             Form::sendForm($player, $form, $this, "onAddCommand");
             $player->sendMessage("§cそのコマンドは既に使用されています");
             return;
         }
-        if($manager->isAdded($data[0])){
+        if ($manager->isAdded($data[0])) {
             $form = $this->getAddCommandForm("§eそのコマンドは既に追加しています§f");
             Form::sendForm($player, $form, $this, "onAddCommand");
             $player->sendMessage("§eそのコマンドは既に追加しています");
             return;
         }
-        if($data[1] === "") $data[1] = "ifPluginで追加したコマンドです";
+        if ($data[1] === "") $data[1] = "ifPluginで追加したコマンドです";
         $manager->set($data[0], [], ["perm" => $data[2] == 0 ? "ifplugin.customcommand.op" : "ifplugin.customcommand.true", "desc" => $data[1], ]);
         $manager->register($data[0], $data[2] == 0 ? "ifplugin.customcommand.op" : "ifplugin.customcommand.true", $data[1]);
-        if($session->getData("action") == "add_empty"){
+        if ($session->getData("action") == "add_empty") {
             $player->sendMessage("追加しました");
             $session->setValid(false);
             return;
@@ -152,23 +153,23 @@ class CommandForm {
 
     public function onSelectCommand($player, $data) {
         $session = Session::get($player);
-        if($data === null) {
+        if ($data === null) {
             $session->setValid(false, false);
             return;
         }
-        if($data[1]) {
+        if ($data[1]) {
             $form = $this->getSelectActionForm();
             Form::sendForm($player, $form, $this, "onSelectAction");
             return;
         }
-        if($data[0] === ""){
+        if ($data[0] === "") {
             $form = $this->getSelectCommandForm("§c必要事項を入力してください§f");
             Form::sendForm($player, $form, $this, "onSelectCommand");
             $player->sendMessage("必要事項を入力してください");
             return;
         }
         $manager = ifPlugin::getInstance()->getCommandManager();
-        if(!$manager->isAdded($data[0])){
+        if (!$manager->isAdded($data[0])) {
             $form = $this->getSelectCommandForm("§cそのコマンドはまだ追加されていません§f");
             Form::sendForm($player, $form, $this, "onSelectCommand");
             $player->sendMessage("そのコマンドはまだ追加されていません");
@@ -177,12 +178,12 @@ class CommandForm {
 
         $session->setData("if_key", $data[0]);
         $action = $session->getData("action");
-        if($action == "edit"){
+        if ($action == "edit") {
             $datas = $manager->get($data[0]);
             $mes = Messages::createMessage($datas["if"], $datas["match"], $datas["else"]);
             $form = (new Form)->getEditIfForm($mes, $datas["name"] ?? null);
             Form::sendForm($player, $form, new Form(), "onEditIf");
-        }elseif($action == "del"){
+        } elseif ($action == "del") {
             $form = (new Form())->getConfirmDeleteForm();
             Form::sendForm($player, $form, new Form(), "onDeleteIf");
         }
@@ -198,7 +199,7 @@ class CommandForm {
         $data = [
             "type" => "form",
             "title" => "command > 操作選択",
-            "content" => "§7ボタンを押してください",
+            "content" => Language::get("form.selectButton"),
             "buttons" => $buttons
         ];
         $json = Form::encodeJson($data);
@@ -207,11 +208,11 @@ class CommandForm {
 
     public function onCommandList($player, $data) {
         $session = Session::get($player);
-        if($data === null) {
+        if ($data === null) {
             $session->setValid(false, false);
             return;
         }
-        if($data === 0) {
+        if ($data === 0) {
             $form = $this->getSelectActionForm();
             Form::sendForm($player, $form, $this, "onSelectAction");
             return;
