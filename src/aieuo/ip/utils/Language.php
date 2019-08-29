b8c3738f9d5ecee5f77f91d144a124af168f9601
aieuo <35859665+aieuo@users.noreply.github.com>
2019-08-29 22:24:28 +0900

    追加

---
+++ src/aieuo/ip/utils/Language.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace aieuo\ip\utils;
+
+class Language {
+
+    public static $messages = [];
+
+    public function __construct($messages) {
+        $this->setMessages($messages);
+    }
+
+    public function setMessages($messages) {
+        self::$messages = $messages;
+    }
+
+    public static function get(string $key, array $replaces = []): string {
+        if (isset(self::$messages[$key])) {
+            $message = self::$messages[$key];
+            foreach ($replaces as $cnt => $value) {
+                $message = str_replace("{%".$cnt."}", $value, $message);
+            }
+            $message = str_replace(["\\n", "\\q", "\\dq"], ["\n", "'", "\""], $message);
+            return $message;
+        }
+        return $key;
+    }
+}
