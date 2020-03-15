4d66a38d76279deabcab4af3215d2ea9fae1680e
aieuo <35859665+aieuo@users.noreply.github.com>
2020-03-15 12:53:31 +0900

    遅れて～するでプレイヤーがオフラインの時にエラーが出る問題を修正

--- src/aieuo/ip/task/DelayedCommandTask.php
+++ src/aieuo/ip/task/DelayedCommandTask.php
@@ -1,17 +1,24 @@
 <?php
 namespace aieuo\ip\task;
 
+use pocketmine\Player;
 use pocketmine\scheduler\Task;
 use pocketmine\command\CommandSender;
 use pocketmine\Server;
 
 class DelayedCommandTask extends Task {
+    /* @var string */
+    private $command;
+    /* @var CommandSender */
+    private $player;
+
     public function __construct(CommandSender $player, string $command) {
         $this->player = $player;
         $this->command = $command;
     }
 
     public function onRun(int $currentTick) {
+        if ($this->player instanceof Player && !$this->player->isOnline()) return;
         Server::getInstance()->dispatchCommand($this->player, $this->command);
     }
 }
