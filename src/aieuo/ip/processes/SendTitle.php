27c8a966fadafeadb5ee5d10fba0a0ccfddea3af
aieuo <35859665+aieuo@users.noreply.github.com>
2020-04-06 20:17:34 +0900

    タイトルの表示時間を変更できるように

--- src/aieuo/ip/processes/SendTitle.php
+++ src/aieuo/ip/processes/SendTitle.php
@@ -13,7 +13,7 @@ class SendTitle extends Process {
     protected $description = "@process.sendtitle.description";
 
     public function getDetail(): string {
-        return Language::get("process.sendtitle.detail", [$this->getTitle(), $this->getSubTitle()]);
+        return Language::get("process.sendtitle.detail", [$this->getTitle(), $this->getSubTitle(), $this->getFadeIn(), $this->getStay(), $this->getFadeOut()]);
     }
 
     public function getTitle(): ?string {
@@ -28,22 +28,43 @@ class SendTitle extends Process {
         return $message;
     }
 
+    public function getFadeIn(): int {
+        return $this->getValues()[2] ?? -1;
+    }
+
+    public function getStay(): int {
+        return $this->getValues()[3] ?? -1;
+    }
+
+    public function getFadeOut(): int {
+        return $this->getValues()[4] ?? -1;
+    }
+
     public function parse(string $content) {
-        $data = explode("[subtitle]", $content);
-        $title = $data[0];
-        $subtitles = $data[1] ?? "";
-        return [$title, $subtitles];
+        $titles = explode("[subtitle]", $content);
+        $title = $titles[0];
+        $subtitles = explode("[fadeIn]", $titles[1] ?? "");
+        $subtitle = $subtitles[0];
+        $fadeIns = explode("[stay]", $subtitles[1] ?? "10[stay]20[fadeOut]10");
+        $fadeIn = $fadeIns[0];
+        $stays = explode("[fadeOut]", $fadeIns[1] ?? "20[fadeOut]10");
+        $stay = $stays[0];
+        $fadeOut = $stays[1];
+        return [$title, $subtitle, (int)$fadeIn, (int)$stay, (int)$fadeOut];
     }
 
     public function execute() {
         $player = $this->getPlayer();
-        $player->addTitle($this->getTitle(), $this->getSubTitle());
+        $player->addTitle($this->getTitle(), $this->getSubTitle(), $this->getFadeIn(), $this->getStay(), $this->getFadeOut());
     }
 
     public function getEditForm(string $default = "", string $mes = "") {
         $messages = $this->parse($default);
         $title = $messages[0];
         $subtitle = $messages[1];
+        $fadeIn = (string)$messages[2];
+        $stay = (string)$messages[3];
+        $fadeOut = (string)$messages[4];
         $data = [
             "type" => "custom_form",
             "title" => $this->getName(),
@@ -51,22 +72,25 @@ class SendTitle extends Process {
                 Elements::getLabel($this->getDescription().(empty($mes) ? "" : "\n".$mes)),
                 Elements::getInput(Language::get("process.sendtitle.form.title"), Language::get("input.example", ["aieuo"]), $title),
                 Elements::getInput(Language::get("process.sendtitle.form.subtitle"), Language::get("input.example", ["aiueo"]), $subtitle),
+                Elements::getInput(Language::get("process.sendtitle.form.fadeIn"), Language::get("input.example", ["-1"]), $fadeIn),
+                Elements::getInput(Language::get("process.sendtitle.form.stay"), Language::get("input.example", ["-1"]), $stay),
+                Elements::getInput(Language::get("process.sendtitle.form.fadeOut"), Language::get("input.example", ["-1"]), $fadeOut),
                 Elements::getToggle(Language::get("form.delete")),
                 Elements::getToggle(Language::get("form.cancel"))
             ]
         ];
-        $json = Form::encodeJson($data);
-        return $json;
+        return Form::encodeJson($data);
     }
 
     public function parseFormData(array $data) {
         $status = true;
         if ($data[1] === "" or $data[2] === "") {
             $status = null;
-        } else {
-            $names = $this->parse($data[1]."[subtitle]".$data[2]);
-            if ($names === false) $status = false;
         }
-        return ["status" => $status, "contents" => $data[1]."[subtitle]".$data[2], "delete" => $data[3], "cancel" => $data[4]];
+        for ($i=3; $i<=5; $i++) {
+            if ($data[$i] === "") $data[$i] = "-1";
+        }
+        $content = $data[1]."[subtitle]".$data[2]."[fadeIn]".$data[3]."[stay]".$data[4]."[fadeOut]".$data[5];
+        return ["status" => $status, "contents" => $content, "delete" => $data[6], "cancel" => $data[7]];
     }
 }
